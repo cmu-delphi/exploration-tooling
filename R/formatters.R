cdc4606d3e6b59974a0f950316bfd7191cc662da
Dmitry Shemetov <dshemetov@ucdavis.edu>
2023-09-21 19:24:41 -0700

    lint: minor spacings

--- R/formatters.R
+++ R/formatters.R
@@ -2,6 +2,7 @@ library(magrittr)
 library(tidyverse)
 library(dplyr)
 library(epipredict)
+
 #' storage format
 #' @description
 #' the columns for any stored prediction are
@@ -10,13 +11,16 @@ library(epipredict)
 #'   separate row.
 format_storage <- function(pred, true_forecast_date) {
   pred %>%
-    mutate(forecast_date = true_forecast_date,
-           .dstn = nested_quantiles(.pred_distn)) %>%
+    mutate(
+      forecast_date = true_forecast_date,
+      .dstn = nested_quantiles(.pred_distn)
+    ) %>%
     unnest(.dstn) %>%
     select(-.pred_distn, -.pred) %>%
     rename(quantile = tau, value = q, target_end_date = target_date) %>%
     relocate(geo_value, forecast_date, target_end_date, quantile, value)
 }
+
 #' format for the COVID-19 Forecast Hub
 #' @description
 #' expects the pred to have the columns geo_value, .pred, .pred_distn, forecast_date, and target_date
