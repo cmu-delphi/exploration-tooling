6ddb930250e5bc26d575ee7b431940ce669c3aa5
dsweber2 <david.weber2@pm.me>
2023-11-01 16:17:47 -0700

    fix: column names check suppression

--- R/epieval-package.R
+++ R/epieval-package.R
@@ -4,6 +4,7 @@
 #' @importFrom purrr transpose map map2_vec
 #' @keywords internal
 "_PACKAGE"
+globalVariables(c("ahead", "id", "parent_id", "all_of", "last_col", "time_value", "geo_value", "target_end_date", "forecast_date", "quantile", ".pred_distn", "quantiles", "quantile_levels", "signal", ".dstn", "values", ".", "forecasters","forecaster", "trainer", "forecast_date", ".pred", "n_distinct", "target_date", "value"))
 .onLoad <- function(libname, pkgname) {
   epidatr::set_cache(
     cache_dir = ".exploration_cache",
