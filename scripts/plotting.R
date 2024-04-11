c39b9987a630194631bfd3d5ed72d8be11bd47a0
Dmitry Shemetov <dshemetov@ucdavis.edu>
2024-04-11 13:18:49 -0700

    feat: basic notebook

--- scripts/plotting.R
+++ scripts/plotting.R
@@ -1,23 +1,19 @@
-# Copied and modified on 2023-09-29 from:
-#   cmu-delphi/flu-hosp-forecast/code/plotting.R
+library(dplyr)
+library(ggplot2)
+library(magrittr)
+library(tidyr)
 
 get_quantiles_df <- function(predictions_cards, intervals = c(.5, .9), ...) {
-  predictions_cards <- predictions_cards %>%
-    dplyr::select(
-      geo_value,
-      quantile,
-      value,
-      forecast_date,
-      target_end_date,
-      forecaster
-    )
+  predictions_cards %<>%
+    dplyr::select(any_of(
+      c("forecaster", "geo_value", "forecast_date", "target_end_date", "quantile", "value")
+    ))
 
   lower_bounds <- predictions_cards %>%
     select(quantile) %>%
     filter(.data$quantile < 0.5) %>%
     unique() %>%
     pull()
-
   quantiles_to_plot <- as.integer(sort(
     round(500L * (1 + intervals %o% c(-1L, 1L)))
   ))
@@ -42,10 +38,8 @@ get_quantiles_df <- function(predictions_cards, intervals = c(.5, .9), ...) {
 
 get_points_df <- function(predictions_cards) {
   points_df <- predictions_cards %>%
-    filter(
-      as.integer(round(.data$quantile * 1000)) == 500L |
-        is.na(.data$quantile)
-    )
+    filter(as.integer(round(.data$quantile * 1000)) == 500L |
+      is.na(.data$quantile))
   if (any(is.na(points_df$quantile))) {
     points_df %<>%
       pivot_wider(names_from = "quantile", values_from = "value") %>%
@@ -72,7 +66,8 @@ plot_quantiles <- function(g, quantiles_df) {
         mapping = aes(
           ymin = .data$lower,
           ymax = .data$upper,
-          group = interaction(.data$forecast_date, .data$forecaster)
+          group = interaction(.data$forecast_date, .data$forecaster),
+          color = NULL
         ),
         alpha = alp[qq]
       )
@@ -87,33 +82,41 @@ plot_points <- function(g, points_df) {
     mapping = aes(
       y = .data$value,
       group = interaction(.data$forecast_date, .data$forecaster)
-    )
+    ),
+    size = 0.125
   )
 
   return(g)
 }
 
-plot_state_forecasters <- function(
-    predictions_cards,
-    exclude_geos = c(),
-    start_day = NULL,
-    ncol = 5) {
-  if (nrow(predictions_cards) == 0) {
-    return(NULL)
-  }
+plot_state_forecasters <- function(predictions_cards, exclude_geos) {
+  assertthat::assert_that(nrow(predictions_cards) > 0)
 
-  predictions_cards %<>%
-    filter(!geo_value %in% exclude_geos)
+  td1 <- epidatr::pub_covidcast(
+    source = "hhs",
+    signals = "confirmed_admissions_covid_1d",
+    geo_type = "state",
+    time_type = "day",
+    geo_values = "*",
+    time_values = epidatr::epirange("2022-01-01", Sys.Date())
+  ) %>%
+    filter(!(.data$geo_value %in% exclude_geos)) %>%
+    dplyr::select(.data$geo_value, .data$time_value, .data$value) %>%
+    dplyr::rename(target_end_date = .data$time_value) %>%
+    mutate(data_source = "hhs")
+  td2 <- epidatr::pub_covidcast(
+    source = "jhu-csse",
+    signals = "confirmed_7dav_incidence_num",
+    geo_type = "state",
+    time_type = "day",
+    geo_values = "*",
+    time_values = epidatr::epirange("2022-01-01", Sys.Date())
+  ) %>%
+    filter(!(.data$geo_value %in% exclude_geos)) %>%
+    dplyr::select(.data$geo_value, .data$time_value, .data$value) %>%
+    dplyr::rename(target_end_date = .data$time_value) %>%
+    mutate(data_source = "jhu")
 
-  td1 <- tar_read("hhs_latest_data_2022") %>%
-    filter(!geo_value %in% exclude_geos) %>%
-    mutate(value = .data$value) %>%
-    rename(target_end_date = time_value)
-  td1$data_source <- "hhs"
-  td2 <- tar_read("chng_latest_data_2022") %>%
-    filter(!geo_value %in% exclude_geos) %>%
-    rename(target_end_date = time_value)
-  td2$data_source <- "chng"
   td1_max <- td1 %>%
     group_by(geo_value) %>%
     summarize(max_value = max(value))
@@ -135,84 +138,66 @@ plot_state_forecasters <- function(
     color = .data$forecaster,
     fill = .data$forecaster
   ))
-
-  points_df <- get_points_df(predictions_cards)
-  g <- plot_points(g, points_df)
-
-  quantiles_df <- get_quantiles_df(predictions_cards)
-  g <- plot_quantiles(g, quantiles_df)
-
-  # Plot truth data by geo
+  g <- plot_points(g, get_points_df(predictions_cards))
+  g <- plot_quantiles(g, get_quantiles_df(predictions_cards))
   g <- g +
     geom_line(mapping = aes(y = .data$value)) +
     geom_line(data = td2, mapping = aes(
       x = .data$target_end_date,
       y = .data$scaled_value
     )) +
-    facet_wrap(~ .data$geo_value, scales = "free_y", ncol = ncol, drop = TRUE) +
+    facet_wrap(~ .data$geo_value, scales = "free_y", ncol = 2, drop = TRUE) +
     theme(legend.position = "top", legend.text = element_text(size = 7))
 
   return(g)
 }
 
-plot_nation_forecasters <- function(
-    predictions_cards,
-    exclude_geos = c(),
-    start_day = NULL,
-    ncol = 5) {
-  if (nrow(predictions_cards) == 0) {
-    return(NULL)
-  }
+plot_nation_forecasters <- function(predictions_cards, exclude_geos) {
+  assertthat::assert_that(nrow(predictions_cards) > 0)
 
-  predictions_cards %<>%
-    filter(!geo_value %in% exclude_geos) %>%
-    group_by(forecaster, forecast_date, quantile, target_end_date) %>%
-    summarize(value = sum(value)) %>%
-    ungroup() %>%
-    mutate(geo_value = "us")
-
-  td1 <- tar_read("hhs_latest_data_2022") %>%
-    filter(!geo_value %in% exclude_geos) %>%
-    group_by(time_value) %>%
-    summarize(value = sum(value)) %>%
-    ungroup() %>%
-    mutate(value = 7L * .data$value) %>%
-    rename(target_end_date = time_value) %>%
-    mutate(geo_value = "us")
-  td1$data_source <- "hhs"
-  td2 <- tar_read("chng_latest_data_2022") %>%
-    filter(!geo_value %in% exclude_geos) %>%
-    group_by(time_value) %>%
-    summarize(value = sum(value)) %>%
-    ungroup() %>%
-    rename(target_end_date = time_value) %>%
-    mutate(geo_value = "us")
-  td2$data_source <- "chng"
-  td1.max <- td1 %>%
+  td1 <- epidatr::pub_covidcast(
+    source = "hhs",
+    signals = "confirmed_admissions_covid_1d",
+    geo_type = "nation",
+    time_type = "day",
+    geo_values = "*",
+    time_values = epidatr::epirange("2022-01-01", Sys.Date())
+  ) %>%
+    filter(!(.data$geo_value %in% exclude_geos)) %>%
+    dplyr::select(.data$time_value, .data$value) %>%
+    dplyr::rename(target_end_date = .data$time_value) %>%
+    mutate(data_source = "hhs")
+  td2 <- epidatr::pub_covidcast(
+    source = "jhu-csse",
+    signals = "confirmed_7dav_incidence_num",
+    geo_type = "nation",
+    time_type = "day",
+    geo_values = "*",
+    time_values = epidatr::epirange("2022-01-01", Sys.Date())
+  ) %>%
+    filter(!(.data$geo_value %in% exclude_geos)) %>%
+    dplyr::select(.data$time_value, .data$value) %>%
+    dplyr::rename(target_end_date = .data$time_value) %>%
+    mutate(data_source = "jhu")
+  td1_max <- td1 %>%
     summarize(max_value = max(value)) %>%
     pull(max_value)
-  td2.max <- td2 %>%
+  td2_max <- td2 %>%
     summarize(max_value = max(value)) %>%
     pull(max_value)
   td2 %<>%
-    mutate(scaled_value = value * td1.max / td2.max)
+    mutate(scaled_value = value * td1_max / td2_max)
 
   # Setup plot
   g <- ggplot(td1, mapping = aes(x = .data$target_end_date))
-
-  quantiles_df <- get_quantiles_df(predictions_cards)
-  g <- plot_quantiles(g, quantiles_df)
-
-  points_df <- get_points_df(predictions_cards)
-  g <- plot_points(g, points_df)
-
-  # Plot truth data by geo
+  g <- plot_quantiles(g, get_quantiles_df(predictions_cards))
+  g <- plot_points(g, get_points_df(predictions_cards))
   g <- g +
     geom_line(mapping = aes(y = .data$value, color = "confirmed admissions")) +
     geom_line(data = td2, mapping = aes(
       x = .data$target_end_date,
       y = .data$scaled_value,
-      color = "chng smoothed_adj_outpatient_flu, scaled"
+      color = "jhu 7day case sum, scaled"
     )) +
     labs(fill = "Reported Signal") +
     theme(legend.position = "top", legend.text = element_text(size = 7))
