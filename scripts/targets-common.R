52b4b03b302eed82f76f3c7c11f3084a8784fd06
Dmitry Shemetov <dshemetov@ucdavis.edu>
2024-05-13 15:08:23 -0700

    fix: update crew settings to the latest working

--- scripts/targets-common.R
+++ scripts/targets-common.R
@@ -2,24 +2,42 @@ suppressPackageStartupMessages({
   source(here::here("R", "load_all.R"))
 })
 
+# On tanka, we have 64 cores, but we leave some free to try to reduce thrashing
+# and to allow for other users.
+if (parallel::detectCores() < 30) {
+  num_workers <- parallel::detectCores() - 1L
+} else {
+  num_workers <- parallel::detectCores() - 20L
+}
+
+main_controller <- crew_controller_local(
+  name = "main_controller",
+  workers = num_cores,
+  # These settings were cobbled together from various discussion threads on the
+  # targets Github. There's been an ongoing issue in a dependency of {crew}
+  # called {mirai}, where workers mysteriously stop working. The settings below
+  # are an attempt to mitigate that.
+  seconds_idle = 60L,
+  seconds_timeout = 7 * 24 * 60 * 60L,
+  tasks_max = 1L,
+  launch_max = 10000L
+)
+
 # The external scores processing causes the pipeline to exit with an error,
 # apparently due to running out of memory. Set up a non-parallel `crew`
 # controller to avoid.
 # https://books.ropensci.org/targets/crew.html#heterogeneous-workers
-main_controller <- crew_controller_local(
-  name = "main_controller",
-  workers = parallel::detectCores() - 1L,
-  seconds_idle = 60L,
-  tasks_max = 1L,
-  launch_max = 10000L
-)
 serial_controller <- crew_controller_local(
   name = "serial_controller",
   workers = 1L,
+  local_log_directory = "local_logs",
   seconds_idle = 60L,
+  seconds_timeout = 7 * 24 * 60 * 60L,
   tasks_max = 1L,
   launch_max = 10000L
 )
+
+# Serial mode is better for debugging.
 debug_mode <- as.logical(Sys.getenv("DEBUG_MODE", "FALSE"))
 if (debug_mode) {
   controllers <- crew_controller_group(serial_controller)
@@ -34,10 +52,13 @@ tar_option_set(
   # https://books.ropensci.org/targets/crew.html#heterogeneous-workers
   resources = tar_resources(
     crew = tar_resources_crew(
-      controller = "main_controller",
-      seconds_timeout = 7 * 24 * 60 * 60
+      controller = "main_controller"
     )
-  )
+  ),
+  memory = "transient",
+  garbage_collection = TRUE,
+  storage = "worker",
+  retrieval = "worker"
 )
 
 linreg <- parsnip::linear_reg()
