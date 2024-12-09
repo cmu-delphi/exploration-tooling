5e49b83c9f6a5eeb3d95a613e6f8e84f5d23a03c
Dmitry Shemetov <dshemetov@ucdavis.edu>
2024-12-09 14:41:43 -0800

    feat: make sure local code is loaded last, so it's not overwritten

--- R/load_all.R
+++ R/load_all.R
@@ -1,3 +1,4 @@
+source(here::here("R", "imports.R"))
 for (file in list.files(here::here("R"), recursive = TRUE, full.names = TRUE)) {
-  if (basename(file) != "load_all.R") source(file)
+  if (!(basename(file) %in% c("imports.R", "load_all.R"))) source(file)
 }
