0b24fcedda846322bbb0288f1364b390f1b8604c
dsweber2 <david.weber2@pm.me>
2026-09-23 14:42:52 -0500

    new datetime

--- R/targets/flu_data_targets.R
+++ R/targets/flu_data_targets.R
@@ -123,7 +123,8 @@ create_flu_data_targets <- function() {
             # HHS geo_type returns two rows per key: fill_method "zero" and "ave".
             # Filter to "ave" before dropping the column.
             filter(is.na(fill_method) | fill_method == "ave") %>%
-            select(geo_value, time_value = reference_time, version = report_time, nssp = value)
+            select(geo_value, time_value = reference_time, version = report_time, nssp = value) %>%
+            mutate(across(c(time_value, version), as.Date))
         }
         nssp <- bind_rows(
           fetch_nssp("state"),
